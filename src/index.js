a8873cc90c02cfdd242220d9f6355f2bc20690b1
GruzinskiiArtem <m1nck@yandex.ru>
2018-09-16 01:51:51 +0300

    Calculation of zeros in the obtained number.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+    var zero = 0;
+    while (number) {
+        number = (number / 5) | 0;
+        zero += number;
+    }
+    return zero;
 }
